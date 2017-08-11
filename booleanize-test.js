32a76ae4a866fc37360b3f3906a54d028da07182
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 12:58:51 +0800

    Updated test.

--- booleanize-test.js
+++ booleanize-test.js
@@ -2,20 +2,18 @@
 const assert = require( "assert" );
 const booleanize = require( "./booleanize.js" );
 
-assert.equal( booleanize( true ), true, "should be equal" );
+assert.equal( booleanize( true ), true, "should be equal to true" );
 
-assert.equal( booleanize( false ), false, "should be equal" );
+assert.equal( booleanize( "true" ), true, "should be equal to true" );
 
-assert.equal( booleanize( "true" ), true, "should be equal" );
+assert.equal( booleanize( "True" ), true, "should be equal to true" );
 
-assert.equal( booleanize( "false" ), false, "should be equal" );
+assert.equal( booleanize( 1 ), true, "should be equal to true" );
 
-assert.equal( booleanize( "True" ), true, "should be equal" );
+assert.equal( booleanize( "false" ), false, "should be equal to false" );
 
-assert.equal( booleanize( "false" ), false, "should be equal" );
+assert.equal( booleanize( false ), false, "should be equal to false" );
 
-assert.equal( booleanize( 1 ), true, "should be equal" );
-
-assert.equal( booleanize( ), false, "should be equal" );
+assert.equal( booleanize( ), false, "should be equal to false" );
 
 console.log( "ok" );
